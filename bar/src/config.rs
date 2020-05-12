aca63127db1d87a2f222cd4950275eadecb0e2ba
eggmund <joshuacolclough2@googlemail.com>
2020-05-12 15:51:41 +0100

    Changed modkey to windows key

--- bar/src/config.rs
+++
@@ -1,5 +0,0 @@
-// Bar config
-pub const BASE_UPDATE_PERIOD: usize = 2;
-// NOTE: These values have to have a non-zero value
-// For an update period of T seconds, you do T/BASE_UPDATE_PERIOD
-pub const TIME_UPDATE_PERIOD: usize = 60/BASE_UPDATE_PERIOD;
